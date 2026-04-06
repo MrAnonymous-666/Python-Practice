8e9b4d10597a347638893f7c3bc0b7f7a36b9f1b
MrAnonymous-666 <kartikpal20062004@gmail.com>
2026-04-06 17:59:26 +0530

    Update practice scripts

--- 2.Type conversion.py
+++ 2.Type conversion.py
@@ -1,8 +1,8 @@
-# Birth_Year = input('Birth Year: ')
-# print(type(Birth_Year))
-# age = 2026 - int(Birth_Year)
-# print(type(age))
-# print(age)
+Birth_Year = input('Birth Year: ')
+print(type(Birth_Year))
+age = 2026 - int(Birth_Year)
+print(type(age))
+print(age)
 
 lbs = input('Weight (lbs):  ')
 kg = int(lbs) *0.45
